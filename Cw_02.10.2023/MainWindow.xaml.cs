e95aedb8a45a0abc29028315910f194e33b3e0eb
flexxdarker <ksenzhakmax@gmail.com>
2023-10-02 21:00:13 +0300

    cw_02.10.2023

---
+++ Cw_02.10.2023/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace Cw_02._10._2023
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public WebClient client = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+        }
+
+        private void downloadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (client.IsBusy)
+            {
+                MessageBox.Show("Try again later!");
+                return;
+            }
+            string width = widthTb.Text;
+            string height = heightTb.Text;
+            string category = typeTb.Text;
+            string url = $"https://source.unsplash.com/random/{height}x{width}/?{category}&1";
+            DownloadFileAsync(url);
+        }
+        private async Task DownloadFileAsync(string url)
+        {
+            string destionation = pathToSaveTb.Text;
+            string name = Path.GetFileName(url);
+            string dest = Path.Combine(destionation, name);
+            await client.DownloadFileTaskAsync(url, dest);
+        }
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+    }
+}
---
+++ Cw_29.09.2023/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Cw_29._09._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 3737;
+            const string JOIN_CMD = "<join>";
+            const string LEAVE_CMD = "<leave>";
+
+            UdpClient server = new(port);
+            // HashSet<> can not contains duplicates
+            HashSet<IPEndPoint> members = new();
+
+            while (true)
+            {
+                IPEndPoint clientIp = null;
+                byte[] data = server.Receive(ref clientIp);
+
+                string message = Encoding.UTF8.GetString(data);
+
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {message} from {clientIp}");
+
+                switch (message)
+                {
+                    case JOIN_CMD:
+                        members.Add(clientIp);
+                        break;
+                    case LEAVE_CMD:
+                        members.Remove(clientIp);
+                        break;
+                    default:
+                        foreach (var m in members)
+                            server.Send(data, m);
+                        break;
+                }
+            }
+        }
+    }
+}
